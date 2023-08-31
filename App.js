4fde5bc4ed066e12f8303500d753e955249bec36
christomah0 <christophemahalomba02@gmail.com>
2023-08-31 16:32:03 +0100

    adding firestore api for manipulating database

--- App.js
+++ App.js
@@ -1,35 +1,31 @@
-import { useCallback } from 'react';
-import {View, Text} from 'react-native';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+// import { useCallback } from 'react';
+// import {View, Text} from 'react-native';
+// import { useFonts } from 'expo-font';
+// import * as SplashScreen from 'expo-splash-screen';
 import ContainerNavigation from "./src/components/navigation-container";
 import './src/i18n';
 
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 
 const App = () => {
-    const [fontsLoaded] = useFonts({
-        'Regular': require('./assets/Fonts/WorkSans-Regular.ttf'),
-        'Medium': require('./assets/Fonts/WorkSans-Medium.ttf'),
-        'SBold': require('./assets/Fonts/WorkSans-SemiBold.ttf'),
-        'Bold': require('./assets/Fonts/WorkSans-Bold.ttf')
-    })
+    // const [fontsLoaded] = useFonts({
+    //     'Regular': require('./assets/Fonts/WorkSans-Regular.ttf'),
+    //     'Medium': require('./assets/Fonts/WorkSans-Medium.ttf'),
+    //     'SBold': require('./assets/Fonts/WorkSans-SemiBold.ttf'),
+    //     'Bold': require('./assets/Fonts/WorkSans-Bold.ttf')
+    // })
 
-    const onLayoutRootView = useCallback(async () => {
-        if(fontsLoaded) {
-            await SplashScreen.hideAsync();
-        }
-    }, [!fontsLoaded]);
+    // const onLayoutRootView = useCallback(async () => {
+    //     if(fontsLoaded) {
+    //         await SplashScreen.hideAsync();
+    //     }
+    // }, [!fontsLoaded]);
   
-    if(!fontsLoaded) {
-        return null;
-    }
+    // if(!fontsLoaded) {
+    //     return null;
+    // }
 
     return <ContainerNavigation />
-    
-    // return <View>
-    //     <Text>Hello</Text>
-    // </View>
 }
 
 export default App;
--- config/firestoreAPI.js
+++ config/firestoreAPI.js
@@ -1,2 +1,28 @@
 import { auth, database } from "./firebase.js";
 import { collection, addDoc, getDoc } from "firebase/firestore";
+
+// Ajout du nouveau post
+async function addNewPost({ content, authorId, createdAt, updatedAt }) {
+    try {
+        if (auth.currentUser != null) {
+            const docRef = await addDoc(collection(database, "users"), {
+                content: content,
+                authorId: authorId,
+                createdAt: createdAt,
+                updatedAt: updatedAt
+            });
+            console.log("Doc written with ID: ", docRef.id);
+        } else {
+            return { msg: "no-auth" };
+        }
+    } catch (err) {
+        console.log("Error adding new doc: ", err);
+    }
+}
+
+// Obtient de tout posts
+// async function getPosts() {
+//     try {
+
+//     }
+// }
