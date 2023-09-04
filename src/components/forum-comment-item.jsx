88c3b68cd6910fca53660db2902fcf1a12be61ed
christomah0 <104055826+christomah0@users.noreply.github.com>
2023-09-04 13:00:53 +0200

    Update forum-comment-item.jsx

--- src/components/forum-comment-item.jsx
+++ src/components/forum-comment-item.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 
@@ -38,45 +37,4 @@ const styles = StyleSheet.create({
     }
 });
 
-=======
-import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
-
-const CommentItem = ({ authorPic, authorName, creationDate, content }) => {
-    return (
-        <View style={{ marginVertical: 10 }}>
-            <View style={ styles.header }>
-                <Image source={ authorPic } style={ styles.img } />
-                <View>
-                    <Text style={ styles.txt }>{ authorName }</Text>
-                    <Text style={ styles.txt }>{ creationDate }</Text>
-                </View>
-            </View>
-            <View>
-                <Text style={ styles.content }>{ content }</Text>
-            </View>
-        </View>
-    );
-}
-
-const styles = StyleSheet.create({
-    header: {
-        flexDirection: "row",
-        gap: 5
-    },
-    img: {
-        width: 24,
-        height: 24
-    },
-    txt: {
-        fontFamily: "Regular"
-    },
-    content: {
-        color: "#ccc",
-        fontFamily: "Regular",
-        marginVertical: 5
-    }
-});
-
->>>>>>> 2947b5bc57dbebe6bacae97819ef1d377410b17e
 export default CommentItem;
